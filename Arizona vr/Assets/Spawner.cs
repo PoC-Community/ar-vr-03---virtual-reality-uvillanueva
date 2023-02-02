d342bd8de43f0832d663b83df97de407ffa1a857
uvillanueva <ulysse.villanueva@epitech.eu>
2023-02-02 16:34:36 +0100

    [FEAT] added basic level structure for game

--- Arizona vr/Assets/Spawner.cs
+++ Arizona vr/Assets/Spawner.cs
@@ -5,23 +5,35 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Vector3[] arr;
+    public float lifetime;
+    public int waveCount;
     public GameObject target;
+    bool b = false;
+    int count;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(StartSpawning());
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Target");
+        int spawned = targets.Length;
+        Debug.Log($"spawned is {spawned}");
+        if (spawned == 0 && count < waveCount)
+            b = false;
+        else
+            b = true;
+        if (b == false)
+            StartCoroutine(StartSpawning());
     }
 
     IEnumerator StartSpawning()
     {
-        Random.Range(0, arr.Length);
-
-        yield return new WaitForSeconds(.1f);
+        Destroy(Instantiate(target, arr[Random.Range(0, arr.Length)], Quaternion.identity), lifetime);
+        count += 1;
+        yield return new WaitForSeconds(3);
     }
 }
